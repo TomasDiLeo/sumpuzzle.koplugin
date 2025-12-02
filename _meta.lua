ee6fdda4401f1702ff5b534708fd763452cc2bab
Tomas Di Leo <tomasjdle@gmail.com>
2025-12-02 20:58:02 -0300

    Description fix

--- _meta.lua
+++ _meta.lua
@@ -3,6 +3,6 @@ local _ = require("gettext")
 return {
     name = "sumpuzzle",
     fullname = _("Sum Puzzle"),
-    description = _("Select numbers from the grid that add up to the target sums shown on the edges. You have 3 lives!"),
+    description = _("Select numbers from the grid that add up to the target sums shown on the edges. You have 3 Hints!"),
     version = "1.0.0",
 }
